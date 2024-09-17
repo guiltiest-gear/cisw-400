cb14718414454cd8f76075224e9711550097886e
Jade Fox <guiltier.gear@gmail.com>
2024-09-16 19:30:57 -0700

    Add week 4 simple slide show files

---
+++ week-4-simple-slide-show/script.js
@@ -0,0 +1,22 @@
+const myImages = [
+  "biden-blast.jpg",
+  "is-he-stupid.jpg",
+  "lebron-fail-heat-action.png",
+  "lebron-fail-ultrakill.jpg",
+];
+var currentImage = 0;
+
+document.getElementById("next").onclick = nextPhoto;
+document.getElementById("previous").onclick = previousPhoto;
+
+function nextPhoto() {
+  currentImage++;
+  if (currentImage > myImages.length - 1) currentImage = 0;
+  document.getElementById("myimage").src = myImages[currentImage];
+}
+
+function previousPhoto() {
+  currentImage--;
+  if (currentImage < 0) currentImage = myImages.length - 1;
+  document.getElementById("myimage").src = myImages[currentImage];
+}
