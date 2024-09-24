5bdb2ef6b0a5e5161b93dcdd56ab803ce08aa7f7
Jade Fox <guiltier.gear@gmail.com>
2024-09-24 15:53:23 -0700

    Add week 5 folding sidebar menus files

---
+++ week-5-folding-sidebar-menus/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function main() {
+  function hideSubmenus() {
+    const submenus = document.querySelectorAll("ul li ul");
+    for (let i = 0; i < submenus.length; i++) {
+      submenus[i].className = "hide-menu";
+    }
+  }
+
+  hideSubmenus();
+
+  const menuLinks = document.querySelectorAll(".menulink");
+  for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      const thisMenu = this.parentNode.querySelector("ul");
+      thisMenu.className = "show-menu";
+      if (thisMenu.classList.contains("hide-menu")) {
+        hideSubmenus();
+        thisMenu.className = "show-menu";
+      } else {
+        thisMenu.className = "hide-menu";
+      }
+    });
+  }
+}
+
+main();
