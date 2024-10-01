b902202b602725f57a3cbd7ed93d323efbaa2401
Jade Fox <guiltier.gear@gmail.com>
2024-10-01 00:47:57 -0700

    Move script into closure

--- week-6-javascript-tabbed-interface/script.js
+++ week-6-javascript-tabbed-interface/script.js
@@ -1,16 +1,17 @@
-"use strict";
+(function () {
+  "use strict";
 
-function main() {
   const tabs = document.querySelectorAll("#tabs > ul > li > a");
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", selectTab);
-  }
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", selectTab);
+  });
 
   function selectTab(event) {
     event.preventDefault();
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].removeAttribute("class");
-    }
+
+    tabs.forEach((tab) => {
+      tab.removeAttribute("class");
+    });
 
     event.target.className = "active";
 
@@ -22,17 +23,15 @@ function main() {
 
     oldTabContent.addEventListener(
       "transitioned",
-      function () {
+      () => {
         oldTabContent.className = "hidden";
 
         thisContent.className = "visible visuallyhidden";
-        setTimeout(function () {
+        setTimeout(() => {
           thisContent.classList.remove("visuallyhidden");
         }, 20);
       },
       { capture: false, once: true, passive: false },
     );
   }
-}
-
-main();
+})();
