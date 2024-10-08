04cd32591545d28ded8ff1bf55734338e9e33b04
Jade Fox <guiltier.gear@gmail.com>
2024-10-07 21:40:10 -0700

    Add week 7 flexslider jquery plugin files

---
+++ week-7-flexslider-jquery-plugin/kittens-basic/script.js
@@ -0,0 +1,9 @@
+$(window).load(function () {
+  $(".flexslider").flexslider({
+    animation: "slide",
+    slideshowSpeed: 2000,
+    direction: "vertical",
+    reverse: true,
+    pauseOnHover: true,
+  });
+});
