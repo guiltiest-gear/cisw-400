4d8a64db43dc9e24544b8acfe4deceb85b7724ae
Jade Fox <guiltier.gear@gmail.com>
2024-10-07 20:49:04 -0700

    Add week 7 sliders on a timer files

---
+++ week-7-sliders-on-a-timer/script.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+$(window).load(function () {
+  "use strict";
+
+  const imageCount = $("#slider ul li").length;
+  const imageWidth = $("#slider ul li:first img").width();
+  const totalWidth = imageCount * imageWidth + "px";
+
+  let leftPosition = 0;
+  let counter = 0;
+
+  $("#slider ul").css("width", totalWidth);
+
+  let mySlider = setInterval(slider, 3000);
+
+  $("#slider").mouseover(function () {
+    clearInterval(mySlider);
+  });
+  $("#slider").mouseout(function () {
+    mySlider = setInterval(slider, 3000);
+  });
+
+  function slider() {
+    counter++;
+    if (counter == imageCount) {
+      counter = 0;
+    }
+    leftPosition = `-${counter * imageWidth}px`;
+    $("#slider ul").animate({ left: leftPosition }, 700, "easeInQuad");
+  }
+});
