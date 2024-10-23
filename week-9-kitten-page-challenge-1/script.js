3cbc8d525dbd4680c8b993a7166caa440797f83a
Jade Fox <guiltier.gear@gmail.com>
2024-10-22 20:22:47 -0700

    Update week 9 kitten page challenge 1 files

---
+++ week-9-kitten-page-challenge-1/script.js
@@ -0,0 +1,18 @@
+$(window).load(function () {
+  $(".flexslider").flexslider({
+    animation: "slide",
+    slideshowSpeed: 5000,
+    pauseOnHover: true,
+    before: function () {
+      $(".cta").css("bottom", "100%");
+    },
+    start: function () {
+      $(".cta").animate({ bottom: "5%" }, 3000, "easeOutElastic");
+    },
+    after: function () {
+      $(".cta").animate({ bottom: "5%" }, 3000, "easeOutElastic");
+    },
+  });
+
+  $(".fancybox").fancybox();
+});
